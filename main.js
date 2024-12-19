0b134775447b6bc8583afaa6b06c3ceeddfeead4
orlando gutierrez <orlandogut5033@gmail.com>
2024-12-19 13:35:11 -0600

    still trying to fix mobile interaction

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const flipCards = document.querySelectorAll('.flip-card');
 
 flipCards.forEach(card => {
-  card.addEventListener('touchstart', function(event) {
-    this.classList.toggle('flip');
-    event.preventDefault(); // Prevent default touch behavior
-  });
+  card.addEventListener('click', toggleFlip);
+  card.addEventListener('touchstart', toggleFlip);
 });
 
+function toggleFlip(event) {
+  event.preventDefault(); // Prevent default link behavior (if clicked on an anchor)
+  this.classList.toggle('flipped');
+}
